e0d7f072d716731b209a932208e358852dbecdea
Dhanush Adithya <s.dhanushadithya@gmail.com>
2023-02-12 20:25:13 +0530

    feat(app): added files for basic auth
    added: routes
        - POST /register
        - POST /login
        - GET /confidential - dummy route to check middleware
    added: controllers
        - register: saves user to DB
        - login: returns JWT token to user
        - signJWT: function to sign JWT for a user
        - confidential: dummy controller
    added: models
        - user (email, password)
    added: middlewares
        - verifyJWT: checks the validity of the user token

---
+++ routes/auth.route.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { confidential, login, register } from "../controllers/auth.controller";
+import { verifyJWT } from "../middlewares/auth.middleware";
+
+const router = Router();
+
+router.get('/confidential', verifyJWT, confidential);
+router.post('/register', register)
+router.post('/login', login)
+
+export default router;
